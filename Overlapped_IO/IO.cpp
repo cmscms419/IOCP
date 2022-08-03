2589ffdef41d37d42e352caf7edd7ff8449e5f80
cmscms419 <72899627+cmscms419@users.noreply.github.com>
2022-08-03 14:59:18 +0900

    commit

--- Overlapped_IO/IO.cpp
+++ Overlapped_IO/IO.cpp
@@ -1,9 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <WinSock2.h>
+
+#define BUF_SIZE 1024
+
+
+void CALLBACK CompRoutine(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+void ErrorHandling(char* msg);
 
 int main()
 {
-	WSASocket();
 	WSABUF data;
+	WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+
+	WSAEVENT event;
+	WSAOVERLAPPED overlapped;
+	WSABUF databuf;
+	char buf[BUF_SIZE] = "안녕하세여!";
+	int recvbytes = 0;
+
+	event = WSACreateEvent();
+	memset(&overlapped, 0, sizeof(overlapped)); // 모든 비트를 0으로 초기화
+	overlapped.hEvent = event;
+	databuf.len = sizeof(buf);
+	databuf.buf = buf;
 
 	return 0;
+}
+
+void CALLBACK CompRoutine(WSABUF databuf,DWORD recvBytes,DWORD dwError, DWORD szRecvBytes, LPWSAOVERLAPPED ipOverlapped, DWORD flags)
+{
+	if (dwError != 0)
+	{
+		printf("Comproutine error\n");
+	}
+	else
+	{
+		recvBytes = szRecvBytes;
+		printf("Received message : %s\n", databuf.buf);
+	}
 }
